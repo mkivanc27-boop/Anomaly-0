65f21c23a11aab32ed352a04308397c10385bb64
mkivanc27-boop <mkivanc27@gmail.com>
2026-05-03 19:48:03 +0300

    Delete src/main/java/com/mbest700/anomaly/mixin/IEntityData.java

--- src/main/java/com/mbest700/anomaly/mixin/IEntityData.java
+++
@@ -1,8 +0,0 @@
-package com.mbest700.anomaly.mixin;
-public interface IEntityData {
-    float getInsanity();
-    void setInsanity(float val);
-    int getVanishTicks();
-    void setVanishTicks(int ticks);
-}
-
